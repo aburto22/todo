1c2fdaff63acc019c1bdc036dc27aa44a49f01e2
aburto22 <aburto22@gmail.com>
2022-06-21 09:06:56 +0200

    added production env

--- src/lib/lists.ts
+++ src/lib/lists.ts
@@ -1,9 +1,9 @@
 import { ITodoList } from '@localTypes/.';
 import { v4 as uuidv4 } from 'uuid';
 
-export const createList = (name: string, owner: string): ITodoList => ({
+export const createList = (name: string, ownerId: string): ITodoList => ({
   name,
-  owner,
+  ownerId,
   id: uuidv4(),
   todos: [],
 });
