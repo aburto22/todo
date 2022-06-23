6957706af6453e90eec0a9a88eb760ee427ee8de
aburto22 <aburto22@gmail.com>
2022-06-23 10:13:24 +0200

    removed graphql playground from production

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,12 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { ApolloServer } from 'apollo-server-micro';
 import typeDefs from '@graphql/typeDefs';
 import resolvers from '@graphql/resolvers';
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { ApolloServerPluginLandingPageGraphQLPlayground, ApolloServerPluginLandingPageDisabled } from 'apollo-server-core';
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  plugins: [
+    process.env.Node_ENV === 'production'
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
 });
 
 export const config = {
